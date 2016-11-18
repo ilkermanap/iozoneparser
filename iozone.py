9ba503b2924f9eb5e448c257dd94f897cb38a619
ilkermanap <ilkermanap@gmail.com>
2016-11-18 18:38:31 +0100

    two line header parsing added

--- iozone.py
+++ iozone.py
@@ -16,7 +16,39 @@ class IozoneRecord:
         self.reclen = reclen
         self.numbytes = 1024 * num
 
+def split_with_location(line, separator=None):
+    temp = {}
+    t = line.strip()
+    finished = False
+    loc = 0
+    while len(t) > 0:
+        if separator is not None:
+            word = t.split(separator)[0]
+        else:
+            word = t.split()[0]
+        loc = line.find(word,loc)        
+        temp[loc] = word
+        loc = loc + len(word)
+        t = line[loc:].strip()
+    return temp
 
+def two_line_headers(line1,line2):
+    up = split_with_location(line1)
+    dn = split_with_location(line2)
+    newdn = dn
+    for loc2 in sorted(dn.keys()):
+        for loc1 in sorted(up.keys()):
+            if overlap(loc2, loc2 + len(dn[loc2]), loc1, loc1 + len(up[loc1])):
+                newdn[loc2] =  up[loc1] +" "+  dn[lo1c2]
+                break
+    return newdn
+
+def overlap(x1,x2, y1,y2):
+    res =False
+    if (x1 > y2) or (x2 < y1):
+        return False
+    return True
+        
 class Iozone:
     def __init__(self, fname):
         self.fname = fname
@@ -42,6 +74,9 @@ class Iozone:
                 else:
                     # we have two line test names, find a way to add 
                     # first parts from the oldline
+                    headers = two_line_headers(oldline, newline)
+                    for k,v in sorted(headers.items()):
+                        print k,v
                     names = newline.strip().split()[2:]
                 break
             else:
@@ -53,4 +88,3 @@ class Iozone:
 
 if __name__ == "__main__":
    f = Iozone("sample.txt")
-
